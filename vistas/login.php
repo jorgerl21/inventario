38f1cc198632722761ef7dc0cc90b14fe011ce87
jorgerl21 <16030025@itecelaya.edu.mx>
2023-08-18 13:03:29 -0600

    inicio de sesion modificacion

--- vistas/login.php
+++ vistas/login.php
@@ -1,4 +1,5 @@
 <div class="main-container">
+
     <form class="box login" action="" method="POST" autocomplete="off">
         <h5 class="title is-5 has-text-centered is-uppercase">Sistema de inventario</h5>
 
@@ -10,22 +11,23 @@
         </div>
 
         <div class="field">
-            <label class="label">Contrase&ntilde;a</label>
+            <label class="label">Clave</label>
             <div class="control">
-                <input class="input" type="password" name="login_clave" pattern="[a-zA-Z0-9$.-@]{7,100}" maxlength="100" required>
+                <input class="input" type="password" name="login_clave" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" required>
             </div>
         </div>
 
         <p class="has-text-centered mb-4 mt-3">
-            <button type="submit" class="button is-info is-rounded">
-                Iniciar Sesion
-            </button>
+            <button type="submit" class="button is-info is-rounded">Iniciar sesion</button>
         </p>
-        <?php 
-            if (isset($_POST['login_ususario']) && isset($_POST['login_clave'])) {
-                require_once "./php/main.php";
-                require_once "./php/iniciar_sesion.php";
-            }
+
+        <?php
+        if (isset($_POST['login_usuario']) && isset($_POST['login_clave'])) {
+            require_once "./php/main.php";
+            require_once "./php/iniciar_sesion.php";
+        }
         ?>
     </form>
+
+
 </div>
