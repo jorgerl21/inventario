34bf78dc472abcba0dd199d1d2d0fda4211c54b1
jorgerl21 <16030025@itecelaya.edu.mx>
2023-09-07 16:59:26 -0600

    commit3

--- php/usuario_eliminar.php
+++ php/usuario_eliminar.php
@@ -1,4 +1,57 @@
 <?php 
     /* Alamcenando datos */
-    $user_id_del = 
+    $user_id_del = limpiar_cadena($_GET['user_id_del']);
+    
+    /* Verificando usuario */
+    $check_usuario = conexion();
+    $check_usuario = $check_usuario -> query("SELECT usuario_id FROM usuario WHERE usuario_id='$user_id_del'");
+
+    if ($check_usuario -> rowCount()==1) {
+        
+        $check_productos = conexion();
+        $check_productos = $check_productos -> query("SELECT usuario_id FROM producto WHERE usuario_id_del='$user_id_del' LIMIT 1");
+
+        if ($check_productos -> rowCount()<=0) {
+            
+            $eliminar_usuario = conexion();
+            $eliminar_usuario = $eliminar_usuario -> prepare("DELETE FROM usuario WHERE usuario_id=:id");
+
+            $eliminar_usuario -> execute([":id" => $user_id_del]);
+
+            if ($eliminar_usuario -> rowCount()==1) {
+                echo '
+                    <div class="notification is-danger is-light">
+                        <strong>¡USUARIO ELIMINADO!</strong><br>
+                        EL USUARIO se elimino con exito
+                    </div>
+                    ';
+            } else {
+                echo '
+                    <div class="notification is-danger is-light">
+                        <strong>¡Ocurrio un error inesperado!</strong><br>
+                        No se pudo eliminar el usuario selecionado por favor intente nuevamente
+                    </div>
+                    ';
+            }
+            $eliminar_usuario = null;
+
+        } else {
+                echo '
+                    <div class="notification is-danger is-light">
+                        <strong>¡Ocurrio un error inesperado!</strong><br>
+                        No podemos eliminar el usuario seleccionado por que tiene productos registrados por el
+                    </div>
+                    ';
+        }
+        $check_productos = null;
+
+    } else {
+        echo '
+            <div class="notification is-danger is-light">
+                <strong>¡Ocurrio un error inesperado!</strong><br>
+                EL USUARIO que intenta eliminar no existe
+            </div>
+            ';
+    }
+    $check_usuario = null;
 ?>
--- vistas/product_list.php
+++ vistas/product_list.php
@@ -1,97 +1,32 @@
-<?php
-	$inicio = ($pagina>0) ? (($pagina * $registros)-$registros) : 0;
-	$tabla="";
+<div class="container id-fluid mb-6">
+    <h1 class="title">Productos</h1>
+    <h2 class="subtitle">Lista de productos</h2>
+</div>
 
-	$campos="producto.producto_id,producto.producto_codigo,producto.producto_nombre,producto.producto_precio,producto.producto_stock,producto.producto_foto,producto.categoria_id,producto.usuario_id,categoria.categoria_id,categoria.categoria_nombre,usuario.usuario_id,usuario.usuario_nombre,usuario.usuario_apellido";
+<div class="container pb-6 pt-6">
+    <?php
+        require_once "./php/main.php";
 
-	if(isset($busqueda) && $busqueda!=""){
+        # Eliminar producto #
+        if(isset($_GET['product_id_del'])){
+            require_once "./php/producto_eliminar.php";
+        }
 
-		$consulta_datos="SELECT $campos FROM producto INNER JOIN categoria ON producto.categoria_id=categoria.categoria_id INNER JOIN usuario ON producto.usuario_id=usuario.usuario_id WHERE producto.producto_codigo LIKE '%$busqueda%' OR producto.producto_nombre LIKE '%$busqueda%' ORDER BY producto.producto_nombre ASC LIMIT $inicio,$registros";
+        if(!isset($_GET['page'])){
+            $pagina=1;
+        }else{
+            $pagina=(int) $_GET['page'];
+            if($pagina<=1){
+                $pagina=1;
+            }
+        }
 
-		$consulta_total="SELECT COUNT(producto_id) FROM producto WHERE producto_codigo LIKE '%$busqueda%' OR producto_nombre LIKE '%$busqueda%'";
+        $pagina=limpiar_cadena($pagina);
+        $url="index.php?vista=product_list&page="; /* <== */
+        $registros=15;
+        $busqueda="";
 
-	}elseif($categoria_id>0){
-
-		$consulta_datos="SELECT $campos FROM producto INNER JOIN categoria ON producto.categoria_id=categoria.categoria_id INNER JOIN usuario ON producto.usuario_id=usuario.usuario_id WHERE producto.categoria_id='$categoria_id' ORDER BY producto.producto_nombre ASC LIMIT $inicio,$registros";
-
-		$consulta_total="SELECT COUNT(producto_id) FROM producto WHERE categoria_id='$categoria_id'";
-
-	}else{
-
-		$consulta_datos="SELECT $campos FROM producto INNER JOIN categoria ON producto.categoria_id=categoria.categoria_id INNER JOIN usuario ON producto.usuario_id=usuario.usuario_id ORDER BY producto.producto_nombre ASC LIMIT $inicio,$registros";
-
-		$consulta_total="SELECT COUNT(producto_id) FROM producto";
-
-	}
-
-	$conexion=conexion();
-
-	$datos = $conexion->query($consulta_datos);
-	$datos = $datos->fetchAll();
-
-	$total = $conexion->query($consulta_total);
-	$total = (int) $total->fetchColumn();
-
-	$Npaginas =ceil($total/$registros);
-
-	if($total>=1 && $pagina<=$Npaginas){
-		$contador=$inicio+1;
-		$pag_inicio=$inicio+1;
-		foreach($datos as $rows){
-			$tabla.='
-				<article class="media">
-			        <figure class="media-left">
-			            <p class="image is-64x64">';
-			            if(is_file("./img/producto/".$rows['producto_foto'])){
-			            	$tabla.='<img src="./img/producto/'.$rows['producto_foto'].'">';
-			            }else{
-			            	$tabla.='<img src="./img/productos.png">';
-			            }
-			   $tabla.='</p>
-			        </figure>
-			        <div class="media-content">
-			            <div class="content">
-			              <p>
-			                <strong>'.$contador.' - '.$rows['producto_nombre'].'</strong><br>
-			                <strong>CODIGO:</strong> '.$rows['producto_codigo'].', <strong>PRECIO:</strong> $'.$rows['producto_precio'].', <strong>STOCK:</strong> '.$rows['producto_stock'].', <strong>CATEGORIA:</strong> '.$rows['categoria_nombre'].', <strong>REGISTRADO POR:</strong> '.$rows['usuario_nombre'].' '.$rows['usuario_apellido'].'
-			              </p>
-			            </div>
-			            <div class="has-text-right">
-			                <a href="index.php?vista=product_img&product_id_up='.$rows['producto_id'].'" class="button is-link is-rounded is-small">Imagen</a>
-			                <a href="index.php?vista=product_update&product_id_up='.$rows['producto_id'].'" class="button is-success is-rounded is-small">Actualizar</a>
-			                <a href="'.$url.$pagina.'&product_id_del='.$rows['producto_id'].'" class="button is-danger is-rounded is-small">Eliminar</a>
-			            </div>
-			        </div>
-			    </article>
-
-			    <hr>
-            ';
-            $contador++;
-		}
-		$pag_final=$contador-1;
-	}else{
-		if($total>=1){
-			$tabla.='
-				<p class="has-text-centered" >
-					<a href="'.$url.'1" class="button is-link is-rounded is-small mt-4 mb-4">
-						Haga clic acá para recargar el listado
-					</a>
-				</p>
-			';
-		}else{
-			$tabla.='
-				<p class="has-text-centered" >No hay registros en el sistema</p>
-			';
-		}
-	}
-
-	if($total>0 && $pagina<=$Npaginas){
-		$tabla.='<p class="has-text-right">Mostrando productos <strong>'.$pag_inicio.'</strong> al <strong>'.$pag_final.'</strong> de un <strong>total de '.$total.'</strong></p>';
-	}
-
-	$conexion=null;
-	echo $tabla;
-
-	if($total>=1 && $pagina<=$Npaginas){
-		echo paginador_tablas($pagina,$Npaginas,$url,7);
-	}
+        # Paginador producto #
+        require_once "./php/producto_lista.php";
+    ?>
+</div>
